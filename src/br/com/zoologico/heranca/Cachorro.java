b0328cd1ac305cae507d4e52a3718619930f0bf8
Adriano Drozdek <61703@PROWAY.TREINA>
2020-09-27 18:55:35 -0300

    adicionando herança no cachorro e no gato

--- src/br/com/zoologico/heranca/Cachorro.java
+++ src/br/com/zoologico/heranca/Cachorro.java
@@ -1,10 +1,8 @@
 package br.com.zoologico.heranca;
 
-public class Cachorro {
+import br.com.zoologico.classes.Animal;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Cachorro extends Animal  {
 
-	}
-
+	
 }
--- src/br/com/zoologico/heranca/Gato.java
+++ src/br/com/zoologico/heranca/Gato.java
@@ -1,10 +1,12 @@
 package br.com.zoologico.heranca;
 
-public class Gato {
+import br.com.zoologico.classes.Animal;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Gato extends Animal {
 
-	}
+	
+		
+
+	
 
 }
