0f85c6c368bfedfe57e7de1b5232c8de060e1bb5
Adriano Drozdek <61703@PROWAY.TREINA>
2020-09-27 16:22:36 -0300

    Instanciando novos objetos

--- src/br/com/zoologico/principal/Main.java
+++ src/br/com/zoologico/principal/Main.java
@@ -5,11 +5,20 @@ import br.com.zoologico.classes.Animal;
 public class Main {
 
 	public static void main(String[] args) {
+		// criando uma nova instancia do objeto
 		Animal a1 = new Animal(); 
 		
 		a1.nome = "Jojo todinho";
 		a1.especie = "Gato";
 		a1.idade = 1;
+		
+		// Criando uma nova instancia do objeto
+		Animal a2 = new Animal();
+		
+		// atribuindo os valores as propriedades
+		a2.nome = "Spike";
+		a2.especie = "Cachorro";
+		a2.idade = 3;
 	}
 	
 }
